d43b4867a13da53d6606113a8c0a3ed6b188999d
“markantonio99” <markomakkia@gmail.com>
2023-03-08 09:30:37 +0100

    salv 2

---
+++ src/components/Main.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="content-movie">
+      <ul class="items_flex">
+        <li class="items_movie" v-for="movie in movies" :key="movie.id">
+          <h2>{{ movie.title }}</h2>
+          <p>Original Title: {{ movie.original_title }}</p>
+          <p>Language: {{ movie.original_language }}</p>
+          <p>Vote: {{ movie.vote_average }}</p>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      movies: {
+        type: Array,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  <style lang="scss" scoped>
+  
+   .items_flex{
+    padding: 40px 0;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 10px;
+  
+    background-color: blue;
+    margin-top: -10px;
+
+    .items_movie{
+        flex-basis: 30%;
+        padding: 20px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 20px;
+        background-color: red;
+        color: white;
+        border: 4px solid black;
+    }
+
+
+
+   }
+   
+ 
+  
+  
+  
+  
+   
+  </style>
